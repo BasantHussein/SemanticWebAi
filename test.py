68f16e28143366c1d38919c8ec4287aa63b68107
BasantHussein <basant_husseinn@yahoo.com>
2020-10-13 22:25:47 +0200

    SemanticWebChatbot

---
+++ test.py
@@ -0,0 +1,185 @@
+import string
+
+from flask import Flask
+from flask import request
+from flask import render_template
+import numpy as np
+import pandas as pd
+import  random
+import re
+
+app = Flask(__name__)
+
+@app.route('/')
+def Form():
+    return render_template('form.html')
+
+@app.route('/search')
+def Search():
+
+    Movies = pd.read_csv("./wiki_movie_plots.csv")
+    Movies = Movies.dropna()
+    horror = Movies[Movies['Genre'] == 'horror']
+    drama = Movies[Movies['Genre'] == 'drama']
+    action = Movies[Movies['Genre'] == 'action']
+    comedy = Movies[Movies['Genre'] == 'comedy']
+    adv = Movies[Movies['Genre'] == 'adventure']
+    res = ""
+    #---function btrg3 esm el movie----
+    def MovieName(list_of_words):
+        if((len(list_of_words))>1):
+            word = ' '.join(list_of_words)
+            return word
+        else:
+            word = list_of_words[0]
+            return word
+    #print("Search here : ")
+    #message = input()
+    message = request.args.get('message', '')
+
+    #--------patterns----------
+
+    allpat = 'all (.*)'
+    horrorpat = 'horror (.*)'
+    dramapat = 'drama (.*)'
+    actionpat = 'action (.*)'
+    comedypat = 'comedy (.*)'
+    advpat = 'adventure (.*)'
+
+    cap = r"[A-Z]\w+"
+    descpat = 'what is the story (.*)'
+    castpat = 'actors|cast (.*)'
+    dicpat = 'director (.*)'
+    kindtpat = 'origin (.*)'
+    yeartpat = 'year|release (.*)'
+    #--matches-----
+
+
+    descmatch = re.search(descpat, message)
+    castmatch = re.search(castpat, message)
+    dictmatch = re.search(dicpat, message)
+    kindmatch = re.search(kindtpat, message)
+    yearmatch = re.search(yeartpat, message)
+    match = re.search(allpat, message)
+    match1 = re.search(horrorpat, message)
+    match2 = re.search(dramapat, message)
+    match3 = re.search(actionpat, message)
+    match4 = re.search(comedypat, message)
+    match5 = re.search(advpat, message)
+
+
+    if match1:
+        print("here is all the horror movies that we recommend to you")
+        res += "here is all the horror movies that we recommend to you"
+        for lab, row in horror.iterrows():
+            print("The Title : " + row['Title'])
+            res += "The Title : " + row['Title']
+            print("The wiki page : " + row['Wiki Page'])
+            res += "The wiki page : " + row['Wiki Page']
+
+    elif match2:
+        print("here is all the drama movies that we recommend to you")
+        res += "here is all the drama movies that we recommend to you"
+        for lab, row in drama.iterrows():
+            print("The Title : " + row['Title'])
+            res += "The Title : " + row['Title']
+            print("The wiki page : " + row['Wiki Page'])
+            res += "The wiki page : " + row['Wiki Page']
+
+    elif match3:
+        print("here is all the action movies that we recommend to you")
+        res += "here is all the action movies that we recommend to you"
+        for lab, row in action.iterrows():
+            rint("The Title : " + row['Title'])
+            res += "The Title : " + row['Title']
+            print("The wiki page : " + row['Wiki Page'])
+            res += "The wiki page : " + row['Wiki Page']
+
+    elif match4:
+        print("here is all the comedy movies that we recommend to you")
+        res += "here is all the comedy movies that we recommend to you"
+        for lab, row in comedy.iterrows():
+            print("The Title : " + row['Title'])
+            res += "The Title : " + row['Title']
+            print("The wiki page : " + row['Wiki Page'])
+            res += "The wiki page : " + row['Wiki Page']
+
+    elif match5:
+        print("here is all the adventure movies that we recommend to you")
+        res += "here is all the adventure movies that we recommend to you"
+        for lab, row in adv.iterrows():
+            print("The Title : " + row['Title'])
+            res += "The Title : " + row['Title']
+            print("The wiki page : " + row['Wiki Page'])
+            res += "The wiki page : " + row['Wiki Page']
+
+    elif descmatch:
+        Mname = re.findall(cap, message)
+        fname = MovieName(Mname)
+        desc = Movies[Movies['Title'] == fname]
+        print("The " + fname + " Story : " + desc['Plot'])
+        res += "The " + fname + " Story : " + desc['Plot']
+
+    elif castmatch:
+        Mname = re.findall(cap, message)
+        fname = MovieName(Mname)
+        desc = Movies[Movies['Title'] == fname]
+        print("The " + fname + " Cast : " + desc['Cast'])
+        res += "The " + fname + " Cast : " + desc['Cast']
+        print("and for more information you can check the wikipage : " + desc['Wiki Page'])
+        res += "and for more information you can check the wikipage : " + desc['Wiki Page']
+
+    elif dictmatch:
+        Mname = re.findall(cap, message)
+        fname = MovieName(Mname)
+        desc = Movies[Movies['Title'] == fname]
+        print("The " + fname + " Director is : " + desc['Director'])
+        res += "The " + fname + " Director is : " + desc['Director']
+        print("and for more information you can check the wikipage : " + desc['Wiki Page'])
+        rse += "and for more information you can check the wikipage : " + desc['Wiki Page']
+
+    elif kindmatch:
+        Mname = re.findall(cap, message)
+        fname = MovieName(Mname)
+        desc = Movies[Movies['Title'] == fname]
+        print(fname + " is " + desc['Origin/Ethnicity'])
+        res += fname + " is " + desc['Origin/Ethnicity']
+        print("and for more information you can check the wikipage : " + desc['Wiki Page'])
+        res += "and for more information you can check the wikipage : " + desc['Wiki Page']
+
+    elif yearmatch:
+        Mname = re.findall(cap, message)
+        fname = MovieName(Mname)
+        desc = Movies[Movies['Title'] == fname]
+        print(fname + " released on  " + desc['Release Year'])
+        res += fname + " released on  " + desc['Release Year']
+        print("and for more information you can check the wikipage : " + desc['Wiki Page'])
+        res += "and for more information you can check the wikipage : " + desc['Wiki Page']
+
+    elif match:
+        print("There's our available movies:D : ")
+        print(Movies['Title'])
+        res += "There's our available movies:D : "
+        res += Movies['Title']
+    else :
+        print("not found")
+        print("but these is our recommendation : Movies['Title']")
+        res += "not found"
+        res += "but these is our recommendation : Movies['Title']"
+
+
+
+    return res
+
+
+
+
+
+
+
+
+
+
+
+
+
